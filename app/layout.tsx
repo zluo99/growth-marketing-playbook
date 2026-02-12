a2d5975926927abc15d12c7f11e105d47729d817
John Luo <jzluo99@gmail.com>
2026-02-11 19:59:06 -0600

    Mobile motion improvements

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,7 @@ export const metadata: Metadata = {
 export const viewport = {
 	width: "device-width",
 	initialScale: 1,
+	viewportFit: "cover",
 }
 
 /* -------------------------------------------------------------------------- */
